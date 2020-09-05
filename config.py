54247e1df991c4e72729839e9b6e4d305e11e759
xerxes <shahikhashayar@gmail.com>
2020-09-05 12:45:38 +0430

    changed config data

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 import urllib2
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fjDpWQ34jfaI8aenC'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
 # Aria2 jsonrpc listening link
 JSONRPC_LINK = 'http://localhost:6800/jsonrpc'
 
 # Aria2 token for rpc comunication
-TOKEN = '3B4NzJn1niVKSEJPuGDY9YE//QMzw7F3oMQL455h+uM='
+TOKEN = 'aria2_token'
 
 # Direction to which downloads will be downloaded
-BASE_DIR = '/home/xerxes/ariadownloads'
+BASE_DIR = '/path/to/downloads'
 
 # Opener used for rpc comunication
 opener = urllib2.build_opener()
